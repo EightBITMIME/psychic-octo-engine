6297af9f2fe9dc39a29530fe811ba981a3196520
dwaynecyr <dcyr@textron.com>
2018-06-25 17:17:10 -0500

    Unit 4 - Private IP (It Verks!)

---
+++ Unit 4/privateip.js
@@ -0,0 +1,20 @@
+$(document).ready(function ubsrt()
+{
+  	window.RTCPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;  
+	var pc = new RTCPeerConnection({iceServers:[]}), 
+	noop = function(){}; 
+     
+   	pc.createDataChannel("");  
+	pc.createOffer(pc.setLocalDescription.bind(pc), noop);   
+    	pc.onicecandidate = function(ice){ 
+   	if(!ice || !ice.candidate || !ice.candidate.candidate)  return;
+
+        	var myIP = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/.exec(ice.candidate.candidate)[1];
+
+        
+	$('.ipAdd').text(myIP);
+  
+        	pc.onicecandidate = noop;
+  
+	 }; 
+});
