486773543214778e25b620fa476119037a728d9e
dwaynecyr <dcyr@textron.com>
2018-06-30 17:47:16 -0500

    Unit 6 Initial

---
+++ Unit 6/privateip.js
@@ -0,0 +1,36 @@
+//Koli, Sarika. (2017, 01, 18)How to get client IP address in JavaScript? retrieved from https://stackoverflow.com/a/41711747
+
+//This script uses WebRTC to identify the clients real, source IP and returns it as an object named ipAdd
+
+
+
+$(document).ready(function ubsrt()
+{
+  	window.RTCPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;  
+	var pc = new RTCPeerConnection({iceServers:[]}), 
+	noop = function(){}; 
+     
+   	pc.createDataChannel("");  
+	pc.createOffer(pc.setLocalDescription.bind(pc), noop);   
+    	pc.onicecandidate = function(ice){ 
+   	if(!ice || !ice.candidate || !ice.candidate.candidate)  return;
+
+        	var myIP = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/.exec(ice.candidate.candidate)[1];
+
+        
+	$('.ipAdd').text(myIP);
+  
+        	pc.onicecandidate = noop;
+  
+	 }; 
+});
+
+/*
+Copyright <YEAR> <COPYRIGHT HOLDER>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
